458017ab07de8abdb9c03a386b43f43a6fc0a595
tarabarnett <taralynnbarnett00@gmail.com>
2024-07-03 11:53:28 -0400

    V0.1 Organizing classes and functions

--- maouenefile.py
+++ maouenefile.py
@@ -1,3 +1,38 @@
-print("Maouene is the best!")
+class ExtractData:
+    def getline(self, transcript):
+        # for each line in the transcript
+        for i in transcript:
+            # searching for just %mor and %umor lines
+            if i.startswith('*MOT') or i.startswith('*CHI') or i.startswith('%mor'):
+                # setting result to a variable
+                # print(i)
+                line = i
+                # sending variable to next function
+                prepareclass = PrepareData()
+                prepareclass.numberlines(line)
 
-x = {}
+
+class PrepareData:
+    # these two functions may be swapped
+    def numberlines(self, all_lines):
+        # number lines of MOT and CHI in file
+        pass
+
+    def makearray(self, line):
+        # here separate words into an array via whitespace
+        pass
+
+class CategorizeLists:
+    def __init__(self):
+        # our grand noun and verb lists
+        grand_nounls = []
+        grand_verbls = []
+
+        # will add other categories here later
+
+
+if __name__ == "__main__":
+    file = open('./amy.cha copy', 'r')
+    lineclass = ExtractData()
+    lineclass.getline(file)
+
